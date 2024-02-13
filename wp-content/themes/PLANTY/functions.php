d94899f9130fb97fcb8966a6d69f2af7baf715d2
kahina <kahina.moly@gmail.com>
2024-02-13 13:38:52 +0100

    Corrections soutenance

--- wp-content/themes/PLANTY/functions.php
+++ wp-content/themes/PLANTY/functions.php
@@ -26,11 +26,11 @@ add_action( 'wp_enqueue_scripts', 'child_enqueue_styles', 15 );
 
 
 
-//Hook qui ajoute un bouton admin au menu quand on est connecté
+//Hook qui ajoute un bouton admin au menu quand on est connecté wordpress
 add_filter( 'wp_nav_menu_items', 'ajout_menu', 10, 2 );
 function ajout_menu( $items, $args ) {
 	if (is_user_logged_in()) {
-		$items .= '<li class="menu-item"><a class="menu-link" href="/wp-admin">Admin</a></li>';
+		$items .= '<li class="menu-item"><a class="menu-link" href="http://localhost/PLANTY/wp-admin/">Admin</a></li>';
 	}
 	return $items;
 
